68d2d5ed6943d110be9a7fb9461535030025f00c
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2024-01-04 12:26:49 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/client/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    compress: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
