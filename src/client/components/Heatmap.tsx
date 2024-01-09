4e89b0f27bf2633050581e7272e2741c6ab7d969
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2024-01-09 08:49:06 -0500

    Make only the squares up to and including the current date clickable

--- src/client/components/Heatmap.tsx
+++ src/client/components/Heatmap.tsx
@@ -10,17 +10,20 @@ function Heatmap(props: HeatMapProps) {
   // const squares: string[] = Array(365).fill("");
   const datesArray: Date[] = [];
 
+  //gets the array of squares (amount of squares)
+  //to see only as many squares up to the current day change endDate to the value of today
   const generateSquaresArray = () => {
     const today = new Date();
     const year = today.getFullYear();
     const startDate = new Date(year, 0, 1);
-    const endDate = today;
+    const endDate = new Date(year, 11, 31);
 
     for(let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
       datesArray.push(new Date(date));
     }
   }
 
+  //gets the columns
   const getChunks = (arr: Date[]) => {
     let chunks = [];
     for (let i = 0; i < arr.length; i += 7) {
--- src/client/components/Square.tsx
+++ src/client/components/Square.tsx
@@ -9,11 +9,13 @@ function Square(props: SquareProps) {
   const [completed, setcompleted] = useState(false);
 
   const clickHandler = () => {
-   setcompleted(!completed);
+    if(props.date <= new Date()){
+      setcompleted(!completed);
+    }
   }
 
   return (
-    <div className='h-4 w-4 rounded cursor-pointer' style={{border: props.date.toDateString() === new Date().toDateString() ? '#FFD700 2px solid' : '', backgroundColor: completed ? '#39D353' : '#161B22'}} onClick={clickHandler}></div>
+    <div className='h-4 w-4 rounded' style={{border: props.date.toDateString() === new Date().toDateString() ? '#FFD700 2px solid' : '', backgroundColor: completed ? '#39D353' : '#161B22', cursor: props.date <= new Date() ? 'pointer' : 'default'}} onClick={clickHandler}></div>
   )
 }
 
