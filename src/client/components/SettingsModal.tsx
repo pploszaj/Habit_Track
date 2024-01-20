fa1c825ace8b393962547dffae046ad7bafeccf3
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2024-01-20 14:54:22 -0500

    Change the color buttons

--- src/client/components/SettingsModal.tsx
+++ src/client/components/SettingsModal.tsx
@@ -10,37 +10,27 @@ function SettingsModal(props: SettingsModalProps) {
     <div className="bg-gray h-48 w-80 z-50 flex flex-col gap-4 justify-center rounded-md items-center left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] fixed">
       <div className="flex gap-4">
         <button
-          className="text-[#D33953]"
+          className="h-6 w-6 rounded-full bg-[#D33953] focus:border focus:border-blue-500"
           onClick={() => props.changeColorHandler("#D33953")}
-        >
-          Red
-        </button>
+        ></button>
         <button
-          className="text-[#39D353]"
+          className="h-6 w-6 rounded-full bg-[#39D353] focus:border focus:border-blue-500"
           onClick={() => props.changeColorHandler("#39D353")}
-        >
-          Green
-        </button>
+        ></button>
         <button
-          className="text-[#3933D3]"
+          className="h-6 w-6 rounded-full bg-[#3933D3] focus:border focus:border-blue-500"
           onClick={() => props.changeColorHandler("#3933D3")}
-        >
-          Blue
-        </button>
+        ></button>
         <button
-          className="text-[#D37339]"
+          className="h-6 w-6 rounded-full bg-[#D37339] focus:border focus:border-blue-500"
           onClick={() => props.changeColorHandler("#D37339")}
-        >
-          Orange
-        </button>
+        ></button>
         <button
-          className="text-[#7339D3]"
+          className="h-6 w-6 rounded-full bg-[#7339D3] focus:border focus:border-blue-500 "
           onClick={() => props.changeColorHandler("#7339D3")}
-        >
-          Purple
-        </button>
+        ></button>
       </div>
-      <button onClick={props.toggleModal}>Close</button>
+      <button onClick={props.toggleModal}>Save</button>
     </div>
   );
 }
